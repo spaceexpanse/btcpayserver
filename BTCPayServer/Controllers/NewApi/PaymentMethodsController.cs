94b908bf13ee1bcc34d895d69b61e9d46dbc92c0
Andrew Camilleri <anca@hesehus.dk>
2018-11-14 16:56:43 +0100

    fix bug with derivations crud

--- BTCPayServer/Controllers/NewApi/PaymentMethodsController.cs
+++ BTCPayServer/Controllers/NewApi/PaymentMethodsController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using BTCPayServer.Payments;
 using BTCPayServer.Security;
 using Microsoft.AspNetCore.Authorization;
@@ -8,20 +9,49 @@ namespace BTCPayServer.Controllers.NewApi
 {
     [ApiController]
     [IncludeInOpenApiDocs]
-    [Route("api/v0.1/stores/{storeId}/[controller]")]
-    [Authorize(Policy = Policies.CanModifyStoreSettings.Key)]
+    [Route("api/v0.1/PaymentMethods")]
     [Authorize()]
     public class PaymentMethodsController : ControllerBase
     {
-
         [HttpGet("")]
         public ActionResult<IEnumerable<string>> GetPaymentMethods()
         {
             return Ok(new List<string>()
             {
-                nameof(PaymentTypes.BTCLike),
-//                nameof(PaymentTypes.LightningLike),
+                nameof(PaymentTypes.BTCLike)
             });
         }
+    }
+    
+    
+    
+    [ApiController]
+    [IncludeInOpenApiDocs]
+    [Route("api/v0.1/PaymentMethods/" + nameof(PaymentTypes.BTCLike))]
+    [Authorize()]
+    public class BTCLikePaymentMethodsController : ControllerBase
+    {
+        private readonly BTCPayNetworkProvider _btcPayNetworkProvider;
+
+        public BTCLikePaymentMethodsController(BTCPayNetworkProvider btcPayNetworkProvider)
+        {
+            _btcPayNetworkProvider = btcPayNetworkProvider;
+        }
+        [HttpGet("")]
+        public ActionResult<IEnumerable<BtcLikePaymentMethod>> GetBtcLikePaymentMethods()
+        {
+            return Ok(_btcPayNetworkProvider
+                .GetAll()
+                .Select(network => new BtcLikePaymentMethod()
+                {
+                    CryptoCode = network.CryptoCode
+                }));
+        }
+    }
+
+    public class BtcLikePaymentMethod
+    {
+        public string CryptoCode { get; set; }
+    
     }
 }
