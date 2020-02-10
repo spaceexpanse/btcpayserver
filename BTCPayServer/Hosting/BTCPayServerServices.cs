917acbe6021549d755353e739a9fa804b8b2f8bc
Kukks <evilkukka@gmail.com>
2020-02-10 14:48:11 +0100

    add nswag docs

--- BTCPayServer/Hosting/BTCPayServerServices.cs
+++ BTCPayServer/Hosting/BTCPayServerServices.cs
@@ -45,8 +45,10 @@ using OpenIddict.EntityFrameworkCore.Models;
 
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.Linq;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using BTCPayServer.Controllers.RestApi;
 using BTCPayServer.Models;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Identity;
@@ -54,6 +56,8 @@ using Microsoft.AspNetCore.Routing;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
 using BTCPayServer.Security.Bitpay;
+using NSwag;
+using NSwag.Generation.Processors.Security;
 using Serilog;
 
 namespace BTCPayServer.Hosting
@@ -261,6 +265,45 @@ namespace BTCPayServer.Hosting
             {
                 options.AddPolicy(CorsPolicies.All, p => p.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin());
             });
+            
+            services.AddSwaggerDocument(config =>
+            {
+                config.PostProcess = document =>
+                {
+                    document.Info.Version = "v1";
+                    document.Info.Title = "BTCPay Greenfield API";
+                    document.Info.Description = "A full API to use your BTCPay Server";
+                    document.Info.TermsOfService = "None";
+                    document.Info.Contact = new NSwag.OpenApiContact
+                    {
+                        Name = "BTCPay Server",
+                        Email = string.Empty,
+                        Url = "https://btcpayserver.org"
+                    };
+                };
+                config.AddOperationFilter(context =>
+                {
+                    var methodInfo = context.MethodInfo;
+                    if (methodInfo != null)
+                    {
+                        return methodInfo.CustomAttributes.Any(data =>
+                                   data.AttributeType == typeof(IncludeInOpenApiDocs)) ||
+                               methodInfo.DeclaringType.CustomAttributes.Any(data =>
+                                   data.AttributeType == typeof(IncludeInOpenApiDocs));
+                    }
+                    return false;
+                });
+                
+                config.AddSecurity("APIKey", Enumerable.Empty<string>(), new OpenApiSecurityScheme
+                {
+                    Type = OpenApiSecuritySchemeType.ApiKey,
+                    Name = "X-APIKEY",
+                    In = OpenApiSecurityApiKeyLocation.Header
+                });
+                
+                config.OperationProcessors.Add(new X("APIKey", AuthenticationSchemes.ApiKey));
+                
+            });
 
             var rateLimits = new RateLimitService();
             rateLimits.SetZone($"zone={ZoneLimits.Login} rate=5r/min burst=3 nodelay");
@@ -294,12 +337,37 @@ namespace BTCPayServer.Hosting
         public static IApplicationBuilder UsePayServer(this IApplicationBuilder app)
         {
             app.UseMiddleware<BTCPayMiddleware>();
+            
+            app.UseOpenApi();
+            app.UseSwaggerUi3();
+            app.UseReDoc(settings => settings.Path= "/docs");
             return app; 
         }
         public static IApplicationBuilder UseHeadersOverride(this IApplicationBuilder app)
         {
             app.UseMiddleware<HeadersOverrideMiddleware>();
             return app;
+        }
+    }
+
+    class X : AspNetCoreOperationSecurityScopeProcessor
+    {
+        private readonly string _authScheme;
+
+        public X(string x, string authScheme): base(x)
+        {
+            _authScheme = authScheme;
+        }
+
+        protected override IEnumerable<string> GetScopes(IEnumerable<AuthorizeAttribute> authorizeAttributes)
+        {
+            var result =  authorizeAttributes
+                .Where(attribute => attribute?.AuthenticationSchemes != null &&  attribute.Policy != null &&
+                                    attribute.AuthenticationSchemes.Equals(_authScheme,
+                                        StringComparison.InvariantCultureIgnoreCase))
+                .Select(attribute => attribute.Policy);
+
+            return result;
         }
     }
 }
