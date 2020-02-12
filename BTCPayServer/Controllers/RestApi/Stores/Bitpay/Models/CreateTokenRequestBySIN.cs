5783fb0acb5a8c433e7a1d7d146b67e80bc76cb9
Kukks <evilkukka@gmail.com>
2020-02-12 17:18:50 +0100

    remove bitpay api management

--- BTCPayServer/Controllers/RestApi/Stores/Bitpay/Models/CreateTokenRequestBySIN.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace BTCPayServer.Controllers.RestApi.Models
-{
-    public class CreateTokenRequestBySIN
-    {
-        public string Label { get; set; }
-    }
-}
