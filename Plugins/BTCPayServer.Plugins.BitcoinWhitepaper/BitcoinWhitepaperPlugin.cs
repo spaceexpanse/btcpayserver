2957de49fb0e8a073443adc2183d8dcde97d1304
Kukks <evilkukka@gmail.com>
2021-01-22 13:21:35 +0100

    Add whitepaper plugin

---
+++ Plugins/BTCPayServer.Plugins.BitcoinWhitepaper/BitcoinWhitepaperPlugin.cs
@@ -0,0 +1,11 @@
+﻿using BTCPayServer.Abstractions.Models;
+
+namespace BTCPayServer.Plugins.BitcoinWhitepaper
+{
+    public class BitcoinWhitepaperPlugin: BaseBTCPayServerPlugin
+    {
+        public  override string Identifier { get; } = "BTCPayServer.Plugins.BitcoinWhitepaper";
+        public  override string Name { get; } = "Bitcoin Whitepaper";
+        public  override string Description { get; } = "This makes the Bitcoin whitepaper available on your BTCPay Server.";
+    }
+}
