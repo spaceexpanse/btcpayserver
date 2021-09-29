bf1eb5a3768a750ab1aed6697bdeee274cdb0d78
Kukks <evilkukka@gmail.com>
2021-09-29 08:00:01 +0200

    Fix typo and do not insert sideshift Js on every checkout if not enabled

--- Plugins/BTCPayServer.Plugins.SideShift/Resources/js/sideShiftComponent.js
+++ Plugins/BTCPayServer.Plugins.SideShift/Resources/js/sideShiftComponent.js
@@ -10,7 +10,7 @@ Vue.component("SideShift",
                     parentAffiliateId: "qg0OrfHJV",
                     defaultSettleMethodId: this.toCurrency,
                     settleAddress: this.toCurrencyAddress,
-                    settleAmount:!this.$parent.srvModel.isUnsetTopUp?  this.toCurrencyDue: undefined``,
+                    settleAmount:!this.$parent.srvModel.isUnsetTopUp?  this.toCurrencyDue: undefined,
                     type: !this.$parent.srvModel.isUnsetTopUp? "fixed": undefined
                 };
                 window.sideshift.show();
