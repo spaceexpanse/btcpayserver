e8edee53ef508cf20366f02eb0ab854a4c355d8f
Wouter Samaey <wouter.samaey@storefront.be>
2022-01-15 21:57:49 +0100

    Rough but working POST /v1/api/custodian-account + GET /v1/api/custodian

--- BTCPayServer/Hosting/Startup.cs
+++ BTCPayServer/Hosting/Startup.cs
@@ -13,6 +13,7 @@ using BTCPayServer.PaymentRequest;
 using BTCPayServer.Plugins;
 using BTCPayServer.Security;
 using BTCPayServer.Services.Apps;
+using BTCPayServer.Services.Custodian;
 using BTCPayServer.Storage;
 using Fido2NetLib;
 using Microsoft.AspNetCore.Authentication;
@@ -110,6 +111,11 @@ namespace BTCPayServer.Hosting
                 };
             });
             services.AddScoped<Fido2Service>();
+            services.AddSingleton<CustodianRegistry>();
+            
+            // TODO Should we keep this here? Other repositories are not listed here, but if I don't put this line, it doesn't work.
+            services.AddSingleton<CustodianAccountRepository>();
+
             services.AddSingleton<UserLoginCodeService>();
 			services.AddSingleton<LnurlAuthService>();
             var mvcBuilder = services.AddMvc(o =>
