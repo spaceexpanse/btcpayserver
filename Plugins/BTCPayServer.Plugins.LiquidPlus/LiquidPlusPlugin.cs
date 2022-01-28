928149222ce442510851e0ab4f09b11d6a2a5a9b
Kukks <evilkukka@gmail.com>
2022-01-28 10:17:22 +0100

    update liquid + sideshift

--- Plugins/BTCPayServer.Plugins.LiquidPlus/LiquidPlusPlugin.cs
+++ Plugins/BTCPayServer.Plugins.LiquidPlus/LiquidPlusPlugin.cs
@@ -25,6 +25,7 @@ namespace BTCPayServer.Plugins.LiquidPlus
 
         public override void Execute(IServiceCollection services)
         {
+            services.AddSingleton<IUIExtension>(new UIExtension("LiquidNav", "store-integrations-nav"));
             services.AddSingleton<IUIExtension>(new UIExtension("CustomLiquidAssetsNavExtension", "server-nav"));
             services.AddSingleton<IUIExtension>(new UIExtension("StoreNavLiquidExtension",  "store-nav"));
             services.AddSingleton<CustomLiquidAssetsRepository>();
