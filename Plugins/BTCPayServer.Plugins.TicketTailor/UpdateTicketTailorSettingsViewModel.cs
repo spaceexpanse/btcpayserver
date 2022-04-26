2b9d495b358d119bf94edec4e070451a2a195e80
Kukks <evilkukka@gmail.com>
2022-04-26 08:06:25 +0200

    TicketTailor Plugin

---
+++ Plugins/BTCPayServer.Plugins.TicketTailor/UpdateTicketTailorSettingsViewModel.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BTCPayServer.Plugins.TicketTailor;
+
+public class UpdateTicketTailorSettingsViewModel
+{
+    public string ApiKey { get; set; }
+    public SelectList Events { get; set; }
+    public string EventId { get; set; }
+    public string WebhookId { get; set; }
+}
