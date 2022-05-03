d8c3583ea80b0f818f3a5ba66c23602b954bdb41
Kukks <evilkukka@gmail.com>
2022-05-03 08:10:26 +0200

    make tt depend on 1.5

--- Plugins/BTCPayServer.Plugins.TicketTailor/TicketTailorPlugin.cs
+++ Plugins/BTCPayServer.Plugins.TicketTailor/TicketTailorPlugin.cs
@@ -14,7 +14,7 @@ namespace BTCPayServer.Plugins.TicketTailor
 
         public override IBTCPayServerPlugin.PluginDependency[] Dependencies { get; } =
         {
-            new() { Identifier = nameof(BTCPayServer), Condition = ">=1.4.9.0" }
+            new() { Identifier = nameof(BTCPayServer), Condition = ">=1.5.0.0" }
         };
 
         public override string Description =>
