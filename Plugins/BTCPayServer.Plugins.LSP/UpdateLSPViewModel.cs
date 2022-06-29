e3ce48f1560d37e0ef448d9dd0b72df8361a8d20
Kukks <evilkukka@gmail.com>
2022-06-29 14:24:28 +0200

    LSP

---
+++ Plugins/BTCPayServer.Plugins.LSP/UpdateLSPViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace BTCPayServer.Plugins.LSP;
+
+public class LSPSettings
+{
+    public bool Enabled { get; set; }
+    public uint Minimum { get; set; }
+    public uint Maximum { get; set; }
+    public decimal FeePerSat { get; set; }
+    public uint BaseFee { get; set; }
+    public string CustomCSS { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+}
+
+public class LSPViewModel
+{
+    public LSPSettings Settings { get; set; }
+}
